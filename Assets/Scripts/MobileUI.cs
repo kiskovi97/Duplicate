458e88939e5e0c7e607d2f545363dc3757a06e4c
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-05-27 15:54:47 +0200

    Updated Scenes and scripts

---
+++ Assets/Scripts/MobileUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_IOS || UNITY_ANDROID
+        
+#else
+        gameObject.SetActive(false);
+#endif
+    }
+}
