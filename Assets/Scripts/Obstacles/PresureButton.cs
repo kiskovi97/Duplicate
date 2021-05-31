bf3953268054ca6f6a70303f484f6fb5ffeb7a83
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-05-31 14:53:02 +0200

    Pressure Button and Level updates

--- Assets/Scripts/Obstacles/PresureButton.cs
+++ Assets/Scripts/Obstacles/PresureButton.cs
@@ -1,20 +1,38 @@
 ﻿using Assets.Scripts.Character;
+using UnityEngine;
 
 namespace Assets.Scripts.Obstacles
 {
     class PresureButton : Obstacle
     {
         public Door door;
+        private int inside = 0;
+
+        private Animator animator;
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
         protected override void DoAnyCollision(CharacterMovement player)
         {
             base.DoAnyCollision(player);
+            inside++;
             door.Open();
+            if (animator != null)
+                animator.SetTrigger("Open");
         }
 
         protected override void DoAnyCollisionExit(CharacterMovement player)
         {
             base.DoAnyCollisionExit(player);
-            door.Close();
+            inside--;
+            if (inside <= 0)
+            {
+                door.Close();
+                if (animator != null)
+                    animator.SetTrigger("Close");
+            }
         }
     }
 }
